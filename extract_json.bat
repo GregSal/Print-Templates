1b412a4578644bf085cefa68b610875967739ec0
Greg Salomons <Gregory.Salomons@kingstonhsc.ca>
2023-02-08 14:09:40 -0500

    added batch file and requirements

---
+++ extract_json.bat
@@ -0,0 +1,5 @@
+call C:\ProgramData\Miniconda3\Scripts\activate.bat C:\ProgramData\Miniconda3
+call conda activate plan_check_tools 
+call cd "Z:\PATIENT DATA\JsonReports"
+Z:
+call pythonw.exe read_json_report.py
